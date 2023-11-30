c58373b74590ce46964c2fd739ef5ff2ea167557
Jev Euniel A. Espada <jevespada12@gmail.com>
2023-11-30 14:56:27 +0800

    I changed the time of switch between image to image.

--- anislideshow.js
+++ anislideshow.js
@@ -2,7 +2,7 @@ window.onload=changeImg;
 
 var i = 0; 			// Start Point
 var images = [];	// Images Array
-var time = 3000;	// Time Between Switch
+var time = 8000;	// Time Between Switch
 	 
 // Image List
 images[0] = "slides/1.png";
