128ea519e095597acbde7dc99c72554e4e150f9f
Jev Euniel A. Espada <jevespada12@gmail.com>
2023-12-01 15:36:24 +0800

    Fixed up teh details and title. I will need to work on the schedule.

--- store-data.js
+++ store-data.js
@@ -1,14 +1,50 @@
 let announcements = [
 {
     "image": "1.png",
-    "Title": "The Anchor App",
-    "details": "Get our free anchor app today by going to ibcd.theanchor.app",
+    "title": "The Anchor App",
+    "details": "Get our free anchor app today by going to ibcd.theanchor.app Through it you can get a personal copy of the KJV Bible. You can tune in to our online radio and hear good and godly music 24/7.",
     "schedule": {}
 },
 {
     "image": "2.png",
-    "Title": "Daily Truth",
+    "title": "Daily Truth",
     "details": "Get your daily dose of biblical truth in our anchor app. Heed to what God wants you to do today.",
+    "schedule": {}
+},
+{
+    "image": "3.png",
+    "title": "SoulWinning App",
+    "details": "Record your soulwinning experience now in our free app.",
+    "schedule": {}
+},
+{
+    "image": "4.png",
+    "title": "Foundations Discipleship",
+    "details": "Grow more by being part of our discipleship program.",
+    "schedule": {}
+},
+{
+    "image": "15.png",
+    "title": "Merry Christmas",
+    "details": "It's only a few more days before Christmas and the New Year is just right after. Get your family into church this December 17 for the Christmas Cantata nad help hear the wonderful story about Christ. Come, hear good and godly music and be reminded of God's love.",
+    "schedule": {}
+},
+{
+    "image": "Church Media.png",
+    "title": "Church Media",
+    "details": "If you missed a service, you can listen to the sermon by going to ibcd.theanchor.app. ",
+    "schedule": {}
+},
+{
+    "image": "It's Ours To Reach.png",
+    "title": "Daily Truth",
+    "details": "Get your daily dose of biblical truth in our anchor app. Heed to what God wants you to do today.",
+    "schedule": {}
+},
+{
+    "image": "Live Programs.png",
+    "title": "Good Morning Dumaguete",
+    "details": "Don't forget to tune in to our online and fm radio every weekday at 7 AM.",
     "schedule": {}
 },
 ]
