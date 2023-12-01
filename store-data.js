d2b9a1e535c9445cf76fc21a7daf40d54ca22fce
Jev Euniel A. Espada <jevespada12@gmail.com>
2023-12-01 14:37:55 +0800

    I added this data for the modal.

--- store-data.js
+++ store-data.js
@@ -1,3 +1,14 @@
-let details = [
-    
+let announcements = [
+{
+    "image": "1.png",
+    "Title": "The Anchor App",
+    "details": "Get our free anchor app today by going to ibcd.theanchor.app",
+    "schedule": {}
+},
+{
+    "image": "2.png",
+    "Title": "Daily Truth",
+    "details": "Get your daily dose of biblical truth in our anchor app. Heed to what God wants you to do today.",
+    "schedule": {}
+},
 ]
