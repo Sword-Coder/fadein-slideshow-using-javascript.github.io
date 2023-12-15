175bb4ad51c7c45d1dd36a36117f6c3616e63d4b
Jev Euniel A. Espada <jevespada12@gmail.com>
2023-12-15 15:03:02 +0800

    I learned that fs doesn't work on live extension servers. This filestoarr.js contains many of my mistaken functions.

--- filestoarr.js
+++ filestoarr.js
@@ -36,6 +36,50 @@ const imageURLs = missionsContent
 
 console.log(imageURLs);*/
 
+//Mistake 3
+/* This works and gives [
+  'slides/missions/1 Tingson.png',
+  'slides/missions/10 LIAN.png',
+  'slides/missions/11 DELA CRUZ.png',
+  'slides/missions/12 GENADA.png',
+  'slides/missions/13 BLANCO.png',
+  'slides/missions/14 ALISAN.png',
+  'slides/missions/15 DE LA CRUZ.png',
+  'slides/missions/16 LASTRA.png',
+  'slides/missions/17 TAGARDA.png',
+  'slides/missions/18 SOLORIANO.png',
+  'slides/missions/19 FUNTECHA.png',
+  'slides/missions/2 Unay.png',
+  'slides/missions/20 Arana.png',
+  'slides/missions/21 NIERVES.png',
+  'slides/missions/22 MANUEL.png',
+  'slides/missions/23 Mampay.png',
+  'slides/missions/24 CAHUAN.png',
+  'slides/missions/25 BANCALE.png',
+  'slides/missions/26 TOLERO.png',
+  'slides/missions/27 Suficiencia.png',
+  'slides/missions/28 CERBAS.png',
+  'slides/missions/29 PISTIN.png',
+  'slides/missions/3 Escaro.png',
+  'slides/missions/30 ARMAH.png',
+  'slides/missions/31 ZACAL.png',
+  'slides/missions/32 BALONZO.png',
+  'slides/missions/33 MONTEALTO.png',
+  'slides/missions/34 DAVE.png',
+  'slides/missions/35 MAGLASANG.png',
+  'slides/missions/36 NARVAEZ.png',
+  'slides/missions/37 SINOY.png',
+  'slides/missions/38 Paguntalan.png',
+  'slides/missions/4 Arteta.png',
+  'slides/missions/5 Nonn.png',
+  'slides/missions/6 ALTURA.png',
+  'slides/missions/7 ANGELES.png',
+  'slides/missions/8 ANDRADA.png',
+  'slides/missions/9 SATUNERO.png'
+]
+*/
+
+/*
 const fs = require("fs");
 const path = require("path");
 
@@ -50,4 +94,32 @@ let missionaries = imageFiles.map((file) =>
   path.join(missionsFolderPath, file)
 );
 
-console.log(imageURLs);
+console.log(missionaries);
+*/
+/*
+const folderPath = "slides"; // Replace with the path to your folder
+
+const missionsFolderPath = `${folderPath}/missions`;
+// Fetch the image URLs from the missions folder
+fetch(missionsFolderPath)
+  .then((response) => response.text())
+  .then((content) => {
+    console.log(content);
+    // Extract the URLs from the content and store them in an array
+    const imageURLs = content
+      .split("\n")
+      .map((line) => line.trim())
+      .filter((line) => line !== "")
+      .map((line) => `${missionsFolderPath}/${line}`);
+
+    console.log(imageURLs);
+  })
+  .catch((error) => console.error(error));
+
+  $(document).ready(function () {
+    $.getJSON("bibleData.json", function (data) {
+      // Use the loaded JSON data here
+      var bibleData = data;
+      // Your code that references bibleData goes here
+    });
+  });*/
